a862a3e53a5fa44c7ce1bfc6fc6c89f053ac5f64
Reuben Staley <lighthousemaniac@gmail.com>
2016-10-26 09:27:47 -0600

    Started work on template for print_lex.

--- print_lex.py
+++ print_lex.py
@@ -8,14 +8,18 @@ ki = lexicon.Lexicon()
 parser = argparse.ArgumentParser(description="Print an entire lexicon or a single term's definitions to the command line.")
 parser.add_argument("lex", help="the lexicon file that should be used (pass a valid JSON filename).")
 parser.add_argument("-q", "--query", nargs=1, help="allows you to enter a query to search. Type the term you are searching for.")
-parser.add_argument("-m", "--method", nargs=1, help="the search method to use.", choices=["index","term","term-part"])
+parser.add_argument("-m", "--method", nargs=1, help="the search method to use.", choices=["index", "term", "term-part"])
+parser.add_argument("-t", "--template", nargs=1, help="which template to use.", choices=["normal", "multiline"])
 args = parser.parse_args()
 
 ki.set_lexicon_file(args.lex)
 
 if args.query == None:
     for i in range(0, len(ki.lexicon)):
-        print(lexicon.format_term(ki.find_term(i)))
+        if args.template != None:
+            print(lexicon.format_term(ki.find_term(i), temp=args.template))
+        else
+            print(lexicon.format_term(ki.find_term(i))
 else:
     if args.method == None or args.method[0].lower() == "term-part":
         terms = ki.find_term(args.query[0], "term-part")
