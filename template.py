84ec60897e7ddbf7ea264a6ab0822890aaf340cf
Reuben Staley <lighthousemaniac@gmail.com>
2016-10-26 09:16:05 -0600

    Add name field

--- template.py
+++ template.py
@@ -1,5 +1,6 @@
 class Template:
-    def __init__(self, before_term="", before_defs=" -- ", before_pos="(", before_list=") ", use_numbers=True, before_def=". ", after_def=" ", after_list="", after=""):
+    def __init__(self, name="default", before_term="", before_defs=" -- ", before_pos="(", before_list=") ", use_numbers=True, before_def=". ", after_def=" ", after_list="", after=""):
+        self.name = name
         self.before_term = before_term
         self.before_defs = before_defs
         self.before_pos = before_pos
@@ -10,4 +11,4 @@ class Template:
         self.after_list = after_list
         self.after = after
         
-multiline = Template(before_defs=":", before_pos="\n  ", before_list="", use_numbers = False, before_def="\n    ", after_list = "\n")
+multiline = Template(name="multiline", before_defs=":", before_pos="\n  ", before_list="", use_numbers = False, before_def="\n    ", after_list = "\n")
